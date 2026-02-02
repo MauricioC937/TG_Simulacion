fe3f5adc36d5992a05e3e0183eb66d6b2351d3e7
MauricioC937 <mauricio.caisaguano@epn.edu.ec>
2026-02-01 20:09:21 -0500

    Actualización de server.R con distribuciones y pérdida agregada
    Se incluyen las restantes distribuciones y el desarrollo de la pérdida agregada

--- TG_Simulacion/server.R
+++ TG_Simulacion/server.R
@@ -1,5 +1,5 @@
 library(shiny)
-
+# Poisson
 x_poisson <- function(lambda, nval){
   res <- numeric(nval)
   for(j in 1:nval){
@@ -15,7 +15,39 @@ x_poisson <- function(lambda, nval){
   }
   return(res)
 }
-
+# Binomial
+x_binomial <- function(n, p, nval){
+  res <- numeric(nval)
+  for(j in 1:nval){
+    c <- p/(1-p)
+    i <- 0
+    Fx <- pr <- (1-p)^n
+    U <- runif(1)
+    while(U > Fx){
+      pr <- c*((n-i)/(i+1))*pr
+      Fx <- Fx + pr
+      i <- i+1
+    }
+    res[j] <- i
+  }
+  return(res)
+}
+# Binomial Negativa
+x_binom_negativa <- function(r,p,nval){
+  res <- numeric(nval)
+  for(j in 1:nval){
+    sum_geom <- 0
+    for(i in 1:r){
+      U <- runif(1)
+      geom <- floor(log(U)/log(1-p)) + 1
+      sum_geom <- sum_geom + geom
+    }
+    res[j] <- sum_geom
+  }
+  return(res)
+}
+# Para Severidad
+# Exponencial
 x_exponencial <- function(lambda, nval){
   res <- numeric(nval)
   for(i in 1:nval){
@@ -23,6 +55,29 @@ x_exponencial <- function(lambda, nval){
   }
   return(res)
 }
+# Gamma
+x_gamma <- function(alpha, lambda, nval){
+  return(rgamma(nval, shape = alpha, rate = lambda))
+}
+#Log-Normal
+x_lognormal <- function(mu, sigma, nval){
+  res <- numeric(nval)
+  for(i in 1:nval){
+    Z <- rnorm(1, mean = mu, sd = sigma)
+    res[i] <- exp(Z)
+  }
+  return(res)
+}
+#weibull
+x_weibull <- function(beta, lambda, nval){
+  res <- numeric(nval)
+  for(i in 1:nval){
+    U <- runif(1)
+    res[i] <- lambda * (-log(1 - U))^(1/beta)
+  }
+  return(res)
+}
+
 
 freq <- function(x){
   cat("• En promedio ocurren", round(mean(x), 2), "siniestros.\n\n")
@@ -43,14 +98,17 @@ sev <- function(x){
 }
 
 function(input, output, session) {
-  
+  #Frecuencia
   frecuencia <- eventReactive(input$sim_freq, {
-    x_poisson(input$lambda_freq, input$n_freq_sim)
+    switch(input$freq_dist,
+          "Poisson" =x_poisson(input$lambda_freq, input$n_freq_sim),
+          "Binomial" = x_binomial(input$n_bin, input$p_bin, input$n_freq_sim),
+          "Binomial Negativa" = x_binom_negativa(input$r_nb, input$p_nb, input$n_freq_sim))
   })
   
   output$freq_plot <- renderPlot({
     hist(frecuencia(),
-         col = "lightblue",
+         col = "#23C23C",
          border = "black",
          main = "Distribución del Número de Siniestros",
          xlab = "Número de siniestros")
@@ -59,14 +117,18 @@ function(input, output, session) {
   output$freq_summary <- renderPrint({
     freq(frecuencia())
   })
-  
+  #Severidad
   severidad <- eventReactive(input$sim_sev, {
-    x_exponencial(input$lambda_sev, input$n_sev_sim)
+    switch(input$sev_dist,
+          "Exponencial" = x_exponencial(input$lambda_exp, input$n_sev_sim),
+          "Gamma" = x_gamma(input$alpha_g, input$lambda_g, input$n_sev_sim),
+          "Log-Normal" = x_lognormal(input$mu_ln, input$sigma_ln, input$n_sev_sim),
+          "Weibull" = x_weibull(input$beta_w, input$lambda_w, input$n_sev_sim))
   })
   
   output$sev_plot <- renderPlot({
     hist(severidad(),
-         col = "lightcyan",
+         col = "#26A4C7",
          border = "black",
          main = "Distribución del Costo del Siniestro",
          xlab = "Costo del siniestro")
@@ -75,6 +137,48 @@ function(input, output, session) {
   output$sev_summary <- renderPrint({
     sev(severidad())
   })
+  # Pérdida Agregada
+  perdida_agregada <- eventReactive(input$sim_total, {
+    
+    M <- input$n_sim_total
+    S <- numeric(M)
+    
+    for (k in 1:M) {
+      
+      Nk <- switch(input$freq_dist,
+                   "Poisson" = x_poisson(input$lambda_freq, 1),
+                   "Binomial" = x_binomial(input$n_bin, input$p_bin, 1),
+                   "Binomial Negativa" = x_binom_negativa(input$r_nb, input$p_nb, 1))
+      
+      if (Nk == 0) {
+        S[k] <- 0
+      } else {
+        Xk <- switch(input$sev_dist,
+                     "Exponencial" = x_exponencial(input$lambda_exp, Nk),
+                     "Gamma" = x_gamma(input$alpha_g, input$lambda_g, Nk),
+                     "Log-Normal" = x_lognormal(input$mu_ln, input$sigma_ln, Nk),
+                     "Weibull" = x_weibull(input$beta_w, input$lambda_w, Nk))
+        S[k] <- sum(Xk)
+      }
+    }
+    return(S)
+  })
   
+  output$agg_plot <- renderPlot({
+    hist(perdida_agregada(), col = "#9C1600", border = "black",
+         main = "Distribución de la Pérdida Agregada",
+         xlab = "Costo total")
+  })
+  
+  output$agg_summary <- renderPrint({
+    S <- perdida_agregada()
+    cat("• El Costo Total de siniestros es de:", round(mean(S), 2), "\n\n")
+    cat("• Un costo típico del siniestro es cercano a:", round(median(S), 2), "\n\n")
+    cat("• En su mayoría el costo del siniestro se encuentra entre",
+        round(quantile(S, 0.1), 2), "y",round(quantile(S, 0.9), 2), "\n")
+    cat("• En escenarios poco frecuentes, el Costo total de siniestros puede llegar hasta:",
+        max(S), ".\n")
+  })
 }
+
 
